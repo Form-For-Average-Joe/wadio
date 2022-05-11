94f25d41cff3ca4589608e01931d90e440dfd904
Chee Heng Koh <cheehengk@r-31-105-25-172.comp.nus.edu.sg>
2022-05-11 15:03:21 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">Form For Average Joe</Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Exercises
+                        </Typography>
+                        <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                            Choose your preferred exercise below!
+                        </Typography>
+                    </Container>
+                </div>
+                <ButtonGroup>
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+            </main>
+        </>
+    );
+}
+
+export default App;
