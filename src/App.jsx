1963a59c11958a555fea720dd0a74196b9122fb3
cheehengk <cheehengk@yahoo.com.sg>
2022-05-12 14:04:36 +0800

    Added styles class

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
-import { PhotoCamera } from '@material-ui/icons';
-
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, TextField, Container } from '@material-ui/core';
+import { FitnessCenter } from '@material-ui/icons';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <>
             <CssBaseline />
             <AppBar position="relative">
                 <Toolbar>
-                    <PhotoCamera />
+                    <FitnessCenter className={classes.icon} />
                     <Typography variant="h6">Form For Average Joe</Typography>
                 </Toolbar>
+                <TextField id="outlined-basic" label="Search" variant="outlined"></TextField>
             </AppBar>
             <main>
-                <div>
-                    <Container maxWidth="sm">
+                <div className={classes.container}>
+                    <Container maxWidth="sm" style={{ marginTop: '70px' }}>
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Exercises
                         </Typography>
                         <Typography variant='h5' align='center' color='textSecondary' paragraph>
                             Choose your preferred exercise below!
                         </Typography>
+                        <div className={classes.buttons}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        Click here!
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        More!
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
-                <ButtonGroup>
-                    <Button>One</Button>
-                    <Button>Two</Button>
-                    <Button>Three</Button>
-                </ButtonGroup>
             </main>
         </>
     );
