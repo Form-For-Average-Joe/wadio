d9557453727d8e4ecd087882feb4a9b50fcf67a8
cheehengk <cheehengk@yahoo.com.sg>
2022-05-17 01:30:11 +0800

    Added basic timer with ref to #14

---
+++ src/Components/Timer.js
@@ -0,0 +1,85 @@
+import { Typography, AppBar, Button, Card, TextField, Grid, FormControl } from '@material-ui/core';
+import useStyles from './styles';
+import React from 'react';
+import { useState, useRef, useEffect } from 'react';
+import StartButton from './StartButton';
+import StopButton from './StopButton';
+
+const duration = 60;
+
+function Timer() {
+    const intervalRef = useRef(null);
+    const [timer, setTimer] = useState('00:00');
+
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        return {
+            total, minutes, seconds
+        };
+    }
+
+    function startTimer(deadline) {
+        let { total, minutes, seconds } = getTimeRemaining(deadline);
+        if (total >= 0) {
+            setTimer(
+                (minutes > 9 ? minutes : '0' + minutes) + ':' +
+                (seconds > 9 ? seconds : '0' + seconds)
+            )
+        } else {
+            clearInterval(intervalRef.current);
+        }
+    }
+
+    function clearTimer(endtime) {
+        setTimer((Math.floor(duration / 60) == 0 ? '00' : Math.floor(duration / 60)) + ':' 
+            + ((duration % 60) == 0 ? '00' : (duration%60)));
+        if (intervalRef.current) clearInterval(intervalRef.current);
+        const id = setInterval(() => {
+            startTimer(endtime);
+        }, 1000)
+        intervalRef.current = id;
+    }
+
+    function getDeadlineTime() {
+        let deadline = new Date();
+        deadline.setSeconds(deadline.getSeconds() + duration);
+        return deadline;
+    }
+
+    useEffect(() => {
+        clearTimer(getDeadlineTime());
+        return () => { if (intervalRef.current) clearInterval(intervalRef.current) }
+    }, []);
+
+    function handleStart() {
+        if (intervalRef.current) clearInterval(intervalRef.current);
+        clearTimer(getDeadlineTime());
+    }
+
+    function handleStop() {
+        if (intervalRef.current) clearInterval(intervalRef.current);
+        clearTimer(getDeadlineTime() - duration);
+    }
+
+    return (
+        <Card>
+            <Grid container spacing={3} direction="column" alignItems="center">
+                <Grid item alignItems="center" style={{ marginTop: "2rem" }}>
+                    <Typography variant="h2"> {timer} </Typography>
+                </Grid>
+                <Grid container spacing={3} justify="center" style={{ marginBottom: "2rem" }}>
+                    <Grid item>
+                        <StartButton handleStart={handleStart}/>
+                    </Grid>
+                    <Grid item>
+                        <StopButton handleStop={handleStop}/>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Card>
+    )
+}
+
+export default Timer;
