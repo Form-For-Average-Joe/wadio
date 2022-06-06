8aaf42844196c0f73813524b6c57152ee916fb38
Parth <67057645+parth-io@users.noreply.github.com>
2022-06-07 00:25:02 +0800

    Uncomment prod code for CI/CD #39

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,35 +1,35 @@
 import {Box, CssBaseline} from "@mui/material";
-import {enableIndexedDbPersistence, initializeFirestore, connectFirestoreEmulator, getFirestore} from "firebase/firestore";
+import {enableIndexedDbPersistence, initializeFirestore} from "firebase/firestore";
 import {Outlet} from "react-router-dom";
 import {AuthProvider, useFirebaseApp, useInitFirestore, FirestoreProvider} from 'reactfire';
-import {getAuth, connectAuthEmulator} from 'firebase/auth';
+import {getAuth} from 'firebase/auth';
 import './App.css'
 
 const App = () => {
   const firebaseApp = useFirebaseApp();
   const auth = getAuth(firebaseApp);
   // When shifting to prod, comment the below
-  const firestoreInstance = getFirestore();
+  // const firestoreInstance = getFirestore();
 
   // When shifting to prod, uncomment the below
-  // const {status, data: firestoreInstance} = useInitFirestore(async (firebaseApp) => {
-  //   const db = initializeFirestore(firebaseApp, {});
-  //   await enableIndexedDbPersistence(db);
-  //   return db;
-  // });
-  //
-  // if (status === 'loading') {
-  //   return (
-  //     <div>Loading</div>
-  //   )
-  // }
+  const {status, data: firestoreInstance} = useInitFirestore(async (firebaseApp) => {
+    const db = initializeFirestore(firebaseApp, {});
+    await enableIndexedDbPersistence(db);
+    return db;
+  });
 
-  if (process.env.NODE_ENV !== 'production') {
-    connectAuthEmulator(auth, 'http://localhost:9099');
-    if (firestoreInstance['_settings']['host'] !== 'localhost:8080') {
-      connectFirestoreEmulator(firestoreInstance, 'localhost', 8080);
-    }
+  if (status === 'loading') {
+    return (
+      <div>Loading</div>
+    )
   }
+  //
+  // if (process.env.NODE_ENV !== 'production') {
+  //   connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+  //   if (firestoreInstance['_settings']['host'] !== 'localhost:8080') {
+  //     connectFirestoreEmulator(firestoreInstance, 'localhost', 8080);
+  //   }
+  // }
 
   return (
     <FirestoreProvider sdk={firestoreInstance}>
