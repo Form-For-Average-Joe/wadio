3bc9dde99ce705a9a9cde2dafe1152b3ac0c17b3
cheehengk <cheehengk@yahoo.com.sg>
2022-06-09 20:40:45 +0800

    Update new exercise end

--- src/containers/AssessmentFinished.js
+++ src/containers/AssessmentFinished.js
@@ -1,50 +1,40 @@
-import { useEffect } from "react";
-import { doc, setDoc, getFirestore } from "firebase/firestore";
-import {Grid, Button} from '@mui/material';
+import { doc, setDoc, getDoc, getFirestore } from "firebase/firestore";
+import { Grid, Card, Typography } from '@mui/material';
+import { getAuth } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import LastAttemptStats from "./LastAttemptStats";
 import { clearExerciseState, selectNameOfExercise, setFeedback } from "../features/exercise/exerciseSlice";
 import { resetStageAndCount, selectCount, selectDuration, setIsCanStart } from '../features/userValues/userValuesSlice'
 import { resetUserTime, selectMinutes, selectSeconds } from '../features/userProfile/userProfileSlice';
 import { useUser } from 'reactfire';
-import {Link, Navigate} from "react-router-dom";
-import exerciseValues from '../poseDetection/values';
+import { Navigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import React from 'react';
+import GenericHeaderButton from "../components/GenericHeaderButton";
+
 
 export default function AssessmentFinished() {
-  const dispatch = useDispatch();
   const repCount = useSelector(selectCount);
   const duration = useSelector(selectDuration);
   const minutes = useSelector(selectMinutes);
   const seconds = useSelector(selectSeconds);
   const nameOfExercise = useSelector(selectNameOfExercise);
+  //const dispatch = useDispatch();
+  const totalCalories = 0;
 
   const { data: user } = useUser();
 
-  useEffect(() => {
-    return () => {
-      if (user) {
-        const sendToFirestore = async () => {
-          await setDoc(doc(getFirestore(), "userStatistics", user.uid), {
-            repCount,
-            workoutTime,
-            nameOfExercise,
-            caloriesBurnt,
-          });
-        }
-        sendToFirestore().then(() => {
-          dispatch(clearExerciseState());
-          dispatch(setIsCanStart(false));
-          dispatch(resetStageAndCount());
-          dispatch(resetUserTime());
-          dispatch(setFeedback(""));
-          exerciseValues.pushupval.isCalibrated = false;
-        })
-      }
-    }
-  }, [])
+  const saveData = () => {
+    setDoc(doc(getFirestore(), "userStatistics", user.uid), {
+      repCount,
+      workoutTime,
+      nameOfExercise,
+      caloriesBurnt,
+    });
+  }
 
-  //todo anonymous user also must persist stats in local cache or online - figure out if need to save in Firebase
-  //todo fetch lastsessionstats from local cache if possible
+  const pastExercise = "/exercise/" + nameOfExercise;
+
   if (!user) {
     //todo remind user to sign in to save stats
     return <Navigate to="/" replace={true} />
@@ -53,18 +43,29 @@ export default function AssessmentFinished() {
   const workoutTime = (minutes * 60 + seconds) === 0 ? duration : duration - (minutes * 60 + seconds);
   const caloriesBurnt = repCount * duration * 0.1;
 
-  // todo write unit tests first
 
   return (
-    <Grid>
-      <Grid item>
-        <LastAttemptStats stats={{ repCount, workoutTime, nameOfExercise, caloriesBurnt }} />
+    <Card sx={{backgroundColor: "#000000"}}>
+      <Grid container spacing={3} direction="column" alignItems="center">
+        <Grid item sx={{marginTop: "3rem"}}>
+          <LastAttemptStats stats={{ repCount, workoutTime, nameOfExercise, caloriesBurnt }} />
+        </Grid>
+        <Grid item>
+          <Typography sx={{color: "#FFFFFF"}}>
+            Select continue to save your workout!
+          </Typography>
+        </Grid>
+        <Grid item>
+          <GenericHeaderButton variant="contained" sx={{ backgroundColor: "#444444" }} onClick={saveData} component={Link} to="/profile">
+            Continue
+          </GenericHeaderButton>
+        </Grid>
+        <Grid item>
+          <GenericHeaderButton variant="contained" sx={{ backgroundColor: "#444444" }} component={Link} to={pastExercise}>
+            Try Again
+          </GenericHeaderButton>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Button variant="contained" component={Link} to="/">
-          Continue
-        </Button>
-      </Grid>
-    </Grid>
+    </Card>
   )
 }
