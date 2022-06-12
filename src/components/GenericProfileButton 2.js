4b9a48078a1c16750e0da594e13fa40b179ba2e6
cheehengk <cheehengk@yahoo.com.sg>
2022-06-12 11:41:44 +0800

    Add initial draft of calorie computation

---
+++ src/components/GenericProfileButton 2.js
@@ -0,0 +1,9 @@
+import {Button} from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const GenericProfileButton = styled(Button)(({ theme }) => ({
+    backgroundColor: "#666666",
+    color: "#FFFFFF",
+}));
+
+export default GenericProfileButton;
--- src/util.js
+++ src/util.js
@@ -20,8 +20,80 @@ export const getFlooredMinutes = (total) => {
   return Math.floor((total / 1000 / 60) % 60);
 }
 
+function getBaseCal(rate, repCount, nameOfExercise) {
+  const rateCorrection = rate * 0.2;
+  const actualCorrection = (repCount * 0.3 < rateCorrection) ? repCount * 0.3 : rateCorrection;
+  return repCount - actualCorrection;
+}
+
+function getWeightCorrectedCal(baseCal, weight) {
+  let cal = baseCal;
+  let benchmark = 85; //set 85kg as benchmark
+  if (weight < benchmark) {
+    while (weight < benchmark) {
+      cal *= 0.96; //calories burnt drops 4% (compounding) per 5kg
+      benchmark -= 5;
+    }
+  }
+  else { //if weight of user is already 85kg, not correction is done
+    while (weight > benchmark) {
+      cal *= 1.04; //calories burnt rises 4% (compounding) per 5kg
+      benchmark += 5;
+    }
+  }
+  return cal;
+}
+
+function getAgeCorrectedCal(baseCal, age) {
+  let cal = baseCal;
+  let benchmark = 25; //set 25 yo as benchmark
+  if (age < benchmark) {
+    while (age < benchmark) {
+      cal *= 0.95; //calories burnt drops 5% (compounding) per 5years
+      benchmark -= 5;
+    }
+  }
+  else { //if weight of user is already 85kg, not correction is done
+    while (age > benchmark) {
+      cal *= 0.95; //calories burnt drops 5% (compounding) per 5years
+      benchmark += 5;
+    }
+  }
+  return cal;
+}
+
+function getFinalCal(baseCal, difficulty, gender) {
+  let cal = baseCal;
+  switch (difficulty) {
+    case 0: //hellish
+      cal *= 1.2; break;
+    case 1: //IPPT
+      cal *= 1.0; break;
+    case 2: //heavenly
+      cal *= 0.8; break;
+    default:
+      console.log("error, invalid difficulty to compute calories"); break;
+  }
+  switch (gender) {
+    case "0": //male
+      cal *= 1.0; break;
+    case "1": //female
+      cal *= 0.9; break;
+    case "2": //others
+      cal *= 0.95; break;
+    default:
+      console.log("error, invalid gender to compute calories"); break;
+  }
+  return cal;
+}
+
 export const getCaloriesBurnt = (repCount, workoutTime, nameOfExercise, difficulty, gender, age, weight) => {
-  return parseInt(age);
+  const rate = repCount / (workoutTime / 60); //rate of exercise, reps per minute
+  const baseCal = getBaseCal(rate, repCount, nameOfExercise);
+  const weightCorrectedCal = getWeightCorrectedCal(baseCal, weight);
+  const ageCorrectedCal = getAgeCorrectedCal(weightCorrectedCal, age);
+  const finalCal = getFinalCal(ageCorrectedCal, difficulty, gender);
+  return finalCal.toFixed(1);
 }
 
 // function handleStart() {
