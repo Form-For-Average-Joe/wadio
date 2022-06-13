153ee9ec4988683bc6184ccd19700501d5ab4eec
cheehengk <cheehengk@yahoo.com.sg>
2022-06-13 20:52:51 +0800

    Delete extra stuff

--- src/components/GenericProfileButton 2.js
+++
@@ -1,9 +0,0 @@
-import {Button} from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-const GenericProfileButton = styled(Button)(({ theme }) => ({
-    backgroundColor: "#666666",
-    color: "#FFFFFF",
-}));
-
-export default GenericProfileButton;
