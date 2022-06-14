636ece519d834325759acdbe11d0c82d217f89a8
cheehengk <cheehengk@yahoo.com.sg>
2022-06-14 17:53:23 +0800

    Add cover and fixes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
-import {Button, Card, CardContent, CardMedia, Container, Grid, Stack, Typography} from '@mui/material';
-import {Link} from "react-router-dom";
+import { Button, Card, CardContent, CardMedia, Container, Grid, Stack, Typography, Box, Paper } from '@mui/material';
+import { Link } from "react-router-dom";
 import pushups from './assets/pushups.jpeg';
 import situps from './assets/situps.png';
+import cover from './assets/cover.jpeg';
 
 const exerciseInformation = [
   {
@@ -25,7 +26,7 @@ const ExerciseCards = () => {
     <Grid container spacing={2}>
       {exerciseInformation.map(exerciseInfo => {
         return (
-          <Grid key={exerciseInfo.title} item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Grid key={exerciseInfo.title} item xs={6} sm={6} md={6} lg={6} xl={6}>
             <Card>
               <CardMedia
                 image={exerciseInfo.image}
@@ -36,11 +37,11 @@ const ExerciseCards = () => {
                 <Typography>
                   {exerciseInfo.description}
                 </Typography>
-                <Stack sx={{paddingTop: 1}}>
+                <Stack sx={{ paddingTop: 1 }}>
                   <Button variant="contained"
-                          style={{backgroundColor: "#0F52BA", color: "#FFFFFF", justifyContent: "center"}}
-                          component={Link}
-                          to={exerciseInfo.to}>Attempt Now!</Button>
+                    style={{ backgroundColor: "#0F52BA", color: "#FFFFFF", justifyContent: "center" }}
+                    component={Link}
+                    to={exerciseInfo.to}>Attempt Now!</Button>
                 </Stack>
               </CardContent>
             </Card>
@@ -53,9 +54,16 @@ const ExerciseCards = () => {
 
 const Home = () => {
   return (
-    <Container sx={{px: {xs: 4, sm: 4, md: 4, lg: 4, xl: 4}, py: {xs: 1, sm: 2, md: 9, lg: 9, xl: 9}}}>
-      <ExerciseCards/>
-    </Container>
+    <Box>
+      <Paper
+        component="img"
+        src={cover}
+        width="100%"
+      />
+      <Container sx={{ px: { xs: 4, sm: 4, md: 4, lg: 4, xl: 4 }, py: { xs: 1, sm: 2, md: 9, lg: 9, xl: 9 } }}>
+        <ExerciseCards />
+      </Container>
+    </Box>
   )
 }
 
