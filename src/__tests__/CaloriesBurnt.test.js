58a8989e2674ad3e48a8243632ad08da1e595735
cheehengk <cheehengk@yahoo.com.sg>
2022-07-04 01:23:46 +0800

    Add test for calories panel

---
+++ src/__tests__/CaloriesBurnt.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import CaloriesBurnt from '../components/CaloriesBurnt';
+
+test('renders react component', () => {
+    render(<CaloriesBurnt cal={1402} />);
+    const currentCal = screen.getByText("1402");
+    const requiredCal = screen.getByText("598")
+    expect(currentCal).toBeInTheDocument();
+    expect(requiredCal).toBeInTheDocument();
+});
+
+test('renders react component', () => {
+    render(<CaloriesBurnt cal={0} />);
+    const currentCal = screen.getByText("0");
+    const requiredCal = screen.getByText("1000")
+    expect(currentCal).toBeInTheDocument();
+    expect(requiredCal).toBeInTheDocument();
+});
+
+test('renders react component', () => {
+    render(<CaloriesBurnt cal={5882} />);
+    const currentCal = screen.getByText("5882");
+    const requiredCal = screen.getByText("0")
+    expect(currentCal).toBeInTheDocument();
+    expect(requiredCal).toBeInTheDocument();
+});
